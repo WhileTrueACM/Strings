7bf4da5c778717ee901ad2d36d22f3672fca2bfb
WhileTrueACM <whiletrueacm@gmail.com>
2016-08-15 14:37:35 +0200

    Update String Alignment.java

--- String Alignment.java
+++ String Alignment.java
@@ -28,11 +28,7 @@
                 dp[i][j] = Math.max(Math.max(up, left), diagonal);
             }
 
-        for (int i = 0; i < x.length + 1; i++) {
-            for (int j = 0; j < y.length + 1; j++)
-                System.out.print(dp[i][j] + "  ");
-            System.out.println();
-        }
+      
 
         return dp[x.length][y.length];
     }
